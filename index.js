a0f8eb57d582bc405f879c4a6af89b3c6a6c327e
KohakuChanX-Dev <102594925+KohakuChan-Dev@users.noreply.github.com>
2023-10-11 11:59:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,26 +7,21 @@ const moment = require("moment");
     const fetchData = await fetch("https://api.github.com/users/Kuuuuuuuu");
     const json = await fetchData.json();
     const text = `
-<h3><b>Hi there, I'm Kuuuuuuuu</b></h3>
+<h3><b>Hi there, I'm Killua24</b></h3>
 <h4>👋 Welcome to my profile!</h4>
 <h4>👀 I'm Student, Developer and Gamer</h4>
 
-<h4>📫 Email: <a href="mailto:me@nayuki.cyou">me@nayuki.cyou</a></h4>
+<h4>📫 Email: <a href="mailto:con@killua.ddns.net">con@killua.ddns.net</a></h4>
 <hr/>
-<a href="https://nayuki.cyou">
-    <img alt="count" src="https://moe-counter.glitch.me/get/@MelidaZ?theme=rule34"/>
-</a>
-<br/>
-<br/>
-<a href="https://wakatime.com/@f0797c6d-4099-4a7f-947c-a8144dcd6348">
-    <img alt="wakatime" src="https://wakatime.com/badge/user/f0797c6d-4099-4a7f-947c-a8144dcd6348.svg"/>
+<a href="https://killua.ddns.net">
+    <img alt="count" src="https://moe-counter.glitch.me/get/@KohakuChan-Dev?theme=rule34"/>
 </a>
 <br/>
 <a href="https://user-badge.committers.top/thailand/Kuuuuuuuu">
     <img alt="wakatime" src="https://user-badge.committers.top/thailand/Kuuuuuuuu.svg"/>
 </a>
 <br/>
-<a href="https://nayuki.cyou">
+<a href="https://killua.ddns.net">
     <img alt="lanyard" src="https://lanyard.cnrad.dev/api/568093374662311956"/>
 </a>
 <br/>
@@ -51,19 +46,19 @@ const moment = require("moment");
     <summary>More Info!</summary>
     <br/>
     <br/>
-    <a href="https://nayuki.cyou">
+    <a href="https://killua.ddns.net">
         <div align="center">
-            <img alt="github" src="https://github-readme-stats.vercel.app/api?username=Kuuuuuuuu&show_icons=true&include_all_commits=true&line_height=28.5&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
+            <img alt="github" src="https://github-readme-stats.vercel.app/api?username=KohakuChan-Dev&show_icons=true&include_all_commits=true&line_height=28.5&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
             <br/>
-            <img alt="github" src="https://github-readme-stats.vercel.app/api/top-langs?username=Kuuuuuuuu&langs_count=15&layout=compact&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
+            <img alt="github" src="https://github-readme-stats.vercel.app/api/top-langs?username=KohakuChan-Dev&langs_count=15&layout=compact&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
             <br/>
-            <img alt="trophy" src="https://github-profile-trophy.vercel.app/?username=Kuuuuuuuu&row=2&column=4&theme=algolia"/>
+            <img alt="trophy" src="https://github-profile-trophy.vercel.app/?username=KohakuChan-Dev&row=2&column=4&theme=algolia"/>
             <br/>
-            <img alt="streak" src="https://github-readme-streak-stats.herokuapp.com/?user=Kuuuuuuuu&theme=dark&background=191970"/>
+            <img alt="streak" src="https://github-readme-streak-stats.herokuapp.com/?user=KohakuChan-Dev&theme=dark&background=191970"/>
             <br/>
-            <img alt="activity" src="https://github-readme-activity-graph.cyclic.app/graph?username=Kuuuuuuuu&bg_color=191970&theme=github"/>
+            <img alt="activity" src="https://github-readme-activity-graph.cyclic.app/graph?username=KohakuChan-Dev&bg_color=191970&theme=github"/>
             <br/>
-            <img alt="repo" src="https://github-contributor-stats.vercel.app/api?username=Kuuuuuuuu&show_icons=true&include_all_commits=true&line_height=28.5&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
+            <img alt="repo" src="https://github-contributor-stats.vercel.app/api?username=KohakuChan-Dev&show_icons=true&include_all_commits=true&line_height=28.5&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
         </div>    
     </a>
 </details>`;
